b28af7bb7b2e0e895cff0c85fa28f9904bc9a5d8
blryface <124838505+blryface@users.noreply.github.com>
2024-01-30 20:35:32 -0300

    remove icon

--- layouts/default.vue
+++ layouts/default.vue
@@ -321,7 +321,7 @@ useSeoMeta({
   ogSiteName: 'nineMinecraft',
   ogDescription: 'Discover and publish Minecraft content!',
   ogType: 'website',
-  ogImage: 'https://avatars.githubusercontent.com/u/154568515?s=400&u=cbe73c8862ad6325359fd98d30b92b06d4e5fd72&v=4',
+  //ogImage: 'https://avatars.githubusercontent.com/u/154568515?s=400&u=cbe73c8862ad6325359fd98d30b92b06d4e5fd72&v=4',
   ogUrl: link,
 
   // Twitter
